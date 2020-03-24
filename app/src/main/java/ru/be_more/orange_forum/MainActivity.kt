b31ae8f28fb01917b5d32a992758475652c6b723
Виталий <zmei@2ch.hk>
2020-03-24 20:16:40 +0700

    Init

---
+++ app/src/main/java/ru/be_more/orange_forum/MainActivity.kt
@@ -0,0 +1,12 @@
+package ru.be_more.orange_forum
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
