6088c2a9d5e17706836998a2410bf2770d9a98b6
Vitaly <zmei@2ch.hk>
2020-08-11 21:48:37 +0700

    forwarding into board from fav

--- app/src/main/java/ru/be_more/orange_forum/ui/favorire/FavoriteBoardViewHolder.kt
+++ app/src/main/java/ru/be_more/orange_forum/ui/favorire/FavoriteBoardViewHolder.kt
@@ -3,15 +3,22 @@ package ru.be_more.orange_forum.ui.favorire
 import android.view.View
 import android.widget.TextView
 import com.thoughtbot.expandablerecyclerview.viewholders.GroupViewHolder
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.item_downloaded_board.*
 import ru.be_more.orange_forum.R
-import ru.be_more.orange_forum.model.Board
-import ru.be_more.orange_forum.model.Category
 
-class FavoriteBoardViewHolder(itemView: View?) : GroupViewHolder(itemView) {
+class FavoriteBoardViewHolder(itemView: View?) : GroupViewHolder(itemView), LayoutContainer {
 
 private var categoryTitle: TextView = itemView!!.findViewById(R.id.tv_downloaded_board_title)
 
+    override val containerView: View?
+        get() = itemView
+
     fun setBoardTitle(boardName: String) {
         categoryTitle.text = boardName
+    }
+
+    fun setIntoBoardListener(listener: View.OnClickListener) {
+        tv_downloaded_board_title.setOnClickListener(listener)
     }
 }
