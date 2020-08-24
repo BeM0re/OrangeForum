6a9b694c776ee7a52765191b831f3a905cbd7764
Vitaly <zmei@2ch.hk>
2020-08-24 22:05:16 +0700

    posting made somehow

--- app/src/main/java/ru/be_more/orange_forum/ui/response/ResponseFragment.kt
+++ app/src/main/java/ru/be_more/orange_forum/ui/response/ResponseFragment.kt
@@ -6,12 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.webkit.CookieManager
+import android.webkit.JavascriptInterface
 import android.webkit.WebSettings
+import androidx.lifecycle.MutableLiveData
 import kotlinx.android.synthetic.main.item_thread_response_form.*
 import moxy.MvpAppCompatFragment
 import moxy.presenter.InjectPresenter
 import ru.be_more.orange_forum.R
-import ru.be_more.orange_forum.ui.thread.ThreadFragment
 
 //TODO вынести в отдельный файл куда-нибудь
 const val PAGE_HTML = "<html>\n" +
@@ -43,10 +44,12 @@ const val PAGE_HTML = "<html>\n" +
         "</html>\n" +
         "\n"
 
-class ResponseFragment: MvpAppCompatFragment(), ResponseView{
+class ResponseFragment(val boardId: String, val threadNum: Int): MvpAppCompatFragment(), ResponseView{
 
     @InjectPresenter(presenterId = "presID", tag = "presTag")
     lateinit var responsePresenter : ResponsePresenter
+
+    val captchaResponse : MutableLiveData<String> = MutableLiveData()
 
     override fun onCreateView(inflater: LayoutInflater,
                               container: ViewGroup?,
@@ -58,8 +61,20 @@ class ResponseFragment: MvpAppCompatFragment(), ResponseView{
 
         setWebView()
 
-    }
+        btn_response_submit.setOnClickListener {
+            posting()
+        }
 
+        captchaResponse.observeForever {token ->
+            responsePresenter.postResponse(
+                boardId,
+                threadNum,
+                et_response_comment.text.toString(),
+                token
+            )
+        }
+
+    }
 
     private fun setWebView(){
 
@@ -88,9 +103,34 @@ class ResponseFragment: MvpAppCompatFragment(), ResponseView{
         wv_post_captcha.settings.defaultTextEncodingName = "utf-8"
         wv_post_captcha.setInitialScale(200)
 
-        wv_post_captcha.loadDataWithBaseURL("https://2ch.hk", PAGE_HTML, "text/html; charset=UTF-8", null, null)
-        wv_post_captcha.addJavascriptInterface(ThreadFragment(), "Android")
+        wv_post_captcha.loadDataWithBaseURL(
+            "https://2ch.hk",
+            PAGE_HTML,
+            "text/html; charset=UTF-8",
+            null,
+            null
+        )
+        wv_post_captcha.addJavascriptInterface(this, "Android")
     }
 
+    private fun posting(){
+        CookieManager.getInstance().flush()
+//        Log.d("M_ThreadFragment", "google.com = "+CookieManager.getInstance().getCookie(".google.com"))
+//        Log.d("M_ThreadFragment", "www.google.com = "+CookieManager.getInstance().getCookie("www.google.com"))
+//        Log.d("M_ThreadFragment", "2ch.hk = "+CookieManager.getInstance().getCookie(".2ch.hk"))
+
+
+
+        wv_post_captcha.loadUrl("javascript: Android.responsePushed(sendParams())")
+
+    }
+
+
+    @JavascriptInterface
+    fun responsePushed(token: String) {
+        Log.d("M_ThreadFragment", "token = $token")
+        captchaResponse.postValue(token.substring(1, token.length-1))
+
+    }
 
 }
