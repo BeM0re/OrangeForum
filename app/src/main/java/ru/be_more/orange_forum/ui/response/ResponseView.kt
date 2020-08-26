4f66caa0aa509d4d7bbe59f8d3d7a2f77063cfdf
Vitaly <zmei@2ch.hk>
2020-08-26 22:15:39 +0700

    Closing response form after posting and showing error.

--- app/src/main/java/ru/be_more/orange_forum/ui/response/ResponseView.kt
+++ app/src/main/java/ru/be_more/orange_forum/ui/response/ResponseView.kt
@@ -1,8 +1,11 @@
 package ru.be_more.orange_forum.ui.response
 
 import moxy.MvpView
+import moxy.viewstate.strategy.AddToEndStrategy
+import moxy.viewstate.strategy.StateStrategyType
 
+@StateStrategyType(value = AddToEndStrategy::class)
 interface ResponseView: MvpView {
-
+    fun closeResponse()
 
 }
