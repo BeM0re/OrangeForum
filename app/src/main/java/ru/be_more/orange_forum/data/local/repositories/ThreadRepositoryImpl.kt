6f7e2f5ec20e70aef5a041481c1c14ee24d5be0c
Vitaly <zmei@2ch.hk>
2020-09-04 20:42:54 +0700

    repo done

--- app/src/main/java/ru/be_more/orange_forum/data/local/repositories/ThreadRepositoryImpl.kt
+++ app/src/main/java/ru/be_more/orange_forum/data/local/repositories/ThreadRepositoryImpl.kt
@@ -1,33 +1,22 @@
 package ru.be_more.orange_forum.data.local.repositories
 
 import android.annotation.SuppressLint
-import android.util.Log
-import io.reactivex.Observable
 import io.reactivex.Single
-import io.reactivex.disposables.Disposable
 import io.reactivex.functions.BiFunction
 import ru.be_more.orange_forum.data.local.LocalContract
-import ru.be_more.orange_forum.data.local.db.dao.BoardDao
-import ru.be_more.orange_forum.data.local.db.dao.PostDao
-import ru.be_more.orange_forum.data.local.db.dao.ThreadDao
+import ru.be_more.orange_forum.data.local.db.dao.DvachDao
 import ru.be_more.orange_forum.data.local.db.entities.StoredBoard
 import ru.be_more.orange_forum.data.local.db.entities.StoredThread
 import ru.be_more.orange_forum.data.local.db.utils.DbConverter.Companion.downloadedToStoredThread
 import ru.be_more.orange_forum.data.local.db.utils.DbConverter.Companion.favoriteToStoredThread
-import ru.be_more.orange_forum.data.local.db.utils.DbConverter.Companion.toModelThread
-import ru.be_more.orange_forum.data.local.db.utils.DbConverter.Companion.toModelThreads
-import ru.be_more.orange_forum.data.local.db.utils.DbConverter.Companion.toStoredFile
-import ru.be_more.orange_forum.data.local.db.utils.DbConverter.Companion.toStoredPost
 import ru.be_more.orange_forum.extentions.processSingle
 import ru.be_more.orange_forum.model.BoardThread
-import ru.be_more.orange_forum.repositories.DvachDbRepository
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class ThreadRepositoryImpl @Inject constructor(
-    private val threadDao: ThreadDao,
-    private val boardDao: BoardDao
+    private val dao: DvachDao
 ) : LocalContract.ThreadRepository {
 
 //    @SuppressLint("CheckResult")
@@ -48,24 +37,24 @@ class ThreadRepositoryImpl @Inject constructor(
     override fun downloadThread(
         thread: BoardThread,
         boardId: String) =
-            Single.zip(boardDao.getBoardCount(boardId),
-                threadDao.getThreadCount(boardId, thread.num),
+            Single.zip(dao.getBoardCount(boardId),
+                dao.getThreadCount(boardId, thread.num),
                 BiFunction <Int, Int, Unit> { boardCount, threadCount ->
                     if (boardCount == 0){
-                        boardDao.insertBoard(StoredBoard(boardId, "", boardId, false))
+                        dao.insertBoard(StoredBoard(boardId, "", boardId, false))
                     }
                     if (threadCount == 0) {
-                        threadDao.insertThread(downloadedToStoredThread(thread, boardId))
+                        dao.insertThread(downloadedToStoredThread(thread, boardId))
                     }
                     else{
-                        threadDao.markThreadDownload(boardId, thread.num)
+                        dao.markThreadDownload(boardId, thread.num)
                     }
                 }
             ).processSingle()
 
 
     override fun deleteThread(boardId: String, threadNum: Int) =
-        Single.fromCallable { threadDao.deleteThread(boardId, threadNum) }
+        Single.fromCallable { dao.deleteThread(boardId, threadNum) }
             .processSingle()
 
 
@@ -75,32 +64,32 @@ class ThreadRepositoryImpl @Inject constructor(
         thread: BoardThread,
         boardId: String,
         boardName: String )=
-        Single.zip(boardDao.getBoardCount(boardId),
-            threadDao.getThreadCount(boardId, thread.num),
+        Single.zip(dao.getBoardCount(boardId),
+            dao.getThreadCount(boardId, thread.num),
             BiFunction <Int, Int, Unit> { boardCount, threadCount ->
                 if (boardCount == 0){
-                    boardDao.insertBoard(StoredBoard(boardId, "", boardName, false))
+                    dao.insertBoard(StoredBoard(boardId, "", boardName, false))
                 }
                 if (threadCount == 0) {
-                    threadDao.insertThread(favoriteToStoredThread(thread, boardId))
+                    dao.insertThread(favoriteToStoredThread(thread, boardId))
                 }
                 else
-                    threadDao.markThreadFavorite(boardId, thread.num)
+                    dao.markThreadFavorite(boardId, thread.num)
             }
         ).processSingle()
 
 
     override fun unmarkThreadFavorite(boardId: String, threadNum: Int) =
-        Single.fromCallable { threadDao.unmarkThreadFavorite(boardId, threadNum) }
+        Single.fromCallable { dao.unmarkThreadFavorite(boardId, threadNum) }
             .processSingle()
 
     override fun markThreadHidden(boardId: String, threadNum: Int) =
-        threadDao.getThreadOrEmpty(boardId, threadNum)
+        dao.getThreadOrEmpty(boardId, threadNum)
             .doOnSuccess { thread ->
                 if (thread.isNotEmpty())
-                    threadDao.markThreadHidden(boardId, threadNum)
+                    dao.markThreadHidden(boardId, threadNum)
                 else
-                    threadDao.insertThread(
+                    dao.insertThread(
                         StoredThread(
                             threadNum,
                             "",
@@ -111,6 +100,6 @@ class ThreadRepositoryImpl @Inject constructor(
             .processSingle()
 
     override fun unmarkThreadHidden(boardId: String, threadNum: Int) =
-        Single.fromCallable { threadDao.unmarkThreadHidden(boardId, threadNum) }
+        Single.fromCallable { dao.unmarkThreadHidden(boardId, threadNum) }
             .processSingle()
 }
