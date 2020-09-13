3ce7640afed5366f385979af3ae127ca2e70d73f
Vitaly <zmei@2ch.hk>
2020-09-13 16:09:38 +0700

    last repo done

--- app/src/main/java/ru/be_more/orange_forum/data/db/repositories/FavoriteRepositoryImpl.kt
+++ app/src/main/java/ru/be_more/orange_forum/data/db/repositories/FavoriteRepositoryImpl.kt
@@ -9,10 +9,14 @@ import ru.be_more.orange_forum.data.db.db.entities.StoredFile
 import ru.be_more.orange_forum.data.db.db.entities.StoredPost
 import ru.be_more.orange_forum.data.db.db.entities.StoredThread
 import ru.be_more.orange_forum.data.db.db.utils.DbConverter.Companion.toModelBoard
+import ru.be_more.orange_forum.data.db.db.utils.DbConverter.Companion.toModelFile
 import ru.be_more.orange_forum.data.db.db.utils.DbConverter.Companion.toModelPost
 import ru.be_more.orange_forum.data.db.db.utils.DbConverter.Companion.toModelThreads
+import ru.be_more.orange_forum.domain.model.AttachFile
 import ru.be_more.orange_forum.extentions.processSingle
 import ru.be_more.orange_forum.domain.model.Board
+import ru.be_more.orange_forum.domain.model.BoardThread
+import ru.be_more.orange_forum.domain.model.Post
 import java.util.*
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -30,31 +34,15 @@ class FavoriteRepositoryImpl @Inject constructor(
             dao.getOpFiles(),
             Function4 <List<StoredBoard>, List<StoredThread>, List<StoredPost>, List<StoredFile>, List<Board>>
             { boards, threads, posts, files ->
-                val boardResult = LinkedList<Board>()
-                boards.forEach{ board ->
-                    val thread = threads.filter { it.boardId == board.id }
-                    if (thread.isNotEmpty())
-                        boardResult.add(toModelBoard(
-                            board,
-                            toModelThreads(thread)
-                        ))
-                    else if (board.isFavorite)
-                        boardResult.add(toModelBoard(
-                            board,
-                            listOf()
-                        ))
-                }
-
-                posts.forEach { post ->
-                    boardResult.find { it.id == post.boardId }
-                        ?.threads?.find { it.num == post.threadNum }
-                        ?.posts = listOf(toModelPost(
-                        post,
-                        files.filter { it.postNum == post.num && it.boardId == post.boardId})
+                boards.map { board ->
+                    toModelBoard(board, toModelThreads(threads)
+                        .map { thread -> thread.copy(posts = posts
+                            .map{ post -> toModelPost(post, files
+                                .filter { it.postNum == post.num }
+                            )}
+                        )}
                     )
                 }
-
-                boardResult
             }
         )
             .processSingle()
--- app/src/main/java/ru/be_more/orange_forum/domain/interactors/FavoriteInteractorImpl.kt
+++ app/src/main/java/ru/be_more/orange_forum/domain/interactors/FavoriteInteractorImpl.kt
@@ -4,13 +4,15 @@ import io.reactivex.Single
 import ru.be_more.orange_forum.data.db.DbContract
 import ru.be_more.orange_forum.domain.InteractorContract
 import ru.be_more.orange_forum.domain.model.Board
+import ru.be_more.orange_forum.extentions.processSingle
 import javax.inject.Inject
 
 class FavoriteInteractorImpl @Inject constructor(
     private val favoriteRepository: DbContract.FavoriteRepository
 ): InteractorContract.FavoriteInteractor {
-    override fun getFavorites(): Single<List<Board>> {
-        TODO("Not yet implemented")
-    }
+
+    override fun getFavorites(): Single<List<Board>> =
+        favoriteRepository.getFavorites()
+            .processSingle()
 
 }
