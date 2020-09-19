b721109d05fe81ac669a80635ad125d9600aa3fc
Vitaly <zmei@2ch.hk>
2020-09-19 20:38:05 +0700

    merge fixed

--- app/src/main/java/ru/be_more/orange_forum/ui/category/CategoryPresenter.kt
+++ app/src/main/java/ru/be_more/orange_forum/ui/category/CategoryPresenter.kt
@@ -1,31 +1,27 @@
 package ru.be_more.orange_forum.ui.category
 
 import android.annotation.SuppressLint
-import ru.be_more.orange_forum.domain.InteractorContract
 import androidx.lifecycle.MutableLiveData
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.Disposable
-import io.reactivex.schedulers.Schedulers
-import moxy.InjectViewState
-import moxy.MvpPresenter
+import ru.be_more.orange_forum.domain.InteractorContract
 import ru.be_more.orange_forum.App
-import ru.be_more.orange_forum.model.Category
-import ru.be_more.orange_forum.repositories.DvachApiRepository
+import ru.be_more.orange_forum.domain.model.Category
 import java.util.*
-import javax.inject.Inject
 
 class CategoryPresenter(
     private val interactor : InteractorContract.CategoryInteractor,
     private var viewState: CategoryView?
 )  {
 
+    private var dataset: MutableLiveData<List<Category>> = MutableLiveData()
 
     //Здесь и в других презентерах Disposable не сохраняется, т.к. он сохраняется в репо
     @SuppressLint("CheckResult")
     fun initPresenter(){
+        dataset.observeForever { data -> viewState?.loadCategories(data) }
+
         interactor.getCategories()
             .subscribe(
-                { viewState?.loadCategories(it) },
+                { dataset.postValue(it) },
                 { App.showToast("Can't load categories") }
             )
     }
@@ -37,7 +33,7 @@ class CategoryPresenter(
 
     fun search(query: String){
         if (query.isEmpty())
-            viewState.loadCategories(dataset.value.orEmpty())
+            viewState?.loadCategories(dataset.value.orEmpty())
         else{
             val filterDataset: LinkedList<Category> = LinkedList()
             for (category in dataset.value!!){
@@ -51,8 +47,8 @@ class CategoryPresenter(
                     )
                 )
             }
-            viewState.loadCategories(filterDataset.filter { it.items.isNotEmpty() })
-            viewState.expandCategories()
+            viewState?.loadCategories(filterDataset.filter { it.items.isNotEmpty() })
+            viewState?.expandCategories()
         }
     }
 }
